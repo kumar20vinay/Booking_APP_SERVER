620d744c9c4e9d05a2f8eaa4d2c2f9858b7e513c
kumar20vinay <vk96154.20@gmail.com>
2024-06-22 21:26:56 +0530

    changes

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ app.set("trust proxy", 1)
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json({limit: "5mb"}))
 app.use(express.urlencoded({extended: true}))
-
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 
